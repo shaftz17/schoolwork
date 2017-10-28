58e7648be3938b2d85ab481fabc30b85d775a663
shaftz17 <aksu.helenius17@gmail.com>
2017-10-28 17:58:04 +0300

    lol

--- KiaTest/KiaTest/Program.cs
+++ KiaTest/KiaTest/Program.cs
@@ -6,7 +6,7 @@ namespace KiaTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("japanese dude jacking off 50 times");
         }
     }
 }
